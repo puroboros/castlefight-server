87b4fa27871149334e828431d65f68297a292895
Pau <paumaru@gmail.com>
2018-11-10 09:57:31 +0100

    initial commit

---
+++ src/main/java/org/castlefight/castlefight/CastlefightApplication.java
@@ -0,0 +1,12 @@
+package org.castlefight.castlefight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastlefightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastlefightApplication.class, args);
+	}
+}
---
+++ src/test/java/org/castlefight/castlefight/CastlefightApplicationTests.java
@@ -0,0 +1,16 @@
+package org.castlefight.castlefight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CastlefightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
